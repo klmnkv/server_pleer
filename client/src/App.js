045ead791507cf6e9f6d3808823b76bed71aa09b
pomka <kolomnikov0405@gmail.com>
2024-07-25 21:46:24 +0700

    test1

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import UploadPage from './components/UploadPage';
+import PlayerPage from './components/PlayerPage';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  const [auth, setAuth] = useState(false);
+
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginPage setAuth={setAuth} />} />
+          <Route
+            path="/upload"
+            element={
+              <ProtectedRoute auth={auth}>
+                <UploadPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/play/:filename" element={<PlayerPage />} />
+          <Route path="/play" element={<PlayerPage />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
